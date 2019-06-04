e6dd0b11e81c902c5ab9f28c40453d5e55897ba7
changqitech <49934060+changqitech@users.noreply.github.com>
2019-06-04 14:45:54 +0800

    Update DefaultMarker.js

--- DefaultMarker.js
+++ DefaultMarker.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, Platform, TouchableHighlight } from 'react-native';
+import PropTypes from 'prop-types';
 
-class DefaultMarker extends React.Component {
+import { View, StyleSheet, Image, Platform, TouchableHighlight } from 'react-native';
+
+const ViewPropTypes = require('react-native').ViewPropTypes || View.propTypes;
+
+export default class DefaultMarker extends React.Component {
+  static propTypes = {
+    pressed: PropTypes.bool,
+    pressedMarkerStyle: ViewPropTypes.style,
+    markerStyle: ViewPropTypes.style,
+    enabled: PropTypes.bool,
+    currentValue: PropTypes.number,
+    valuePrefix: PropTypes.string,
+    valueSuffix: PropTypes.string,
+  };
+
   render() {
     return (
       <TouchableHighlight>
         <View
-          style={
-            this.props.enabled
-              ? [
-                  styles.markerStyle,
-                  this.props.markerStyle,
-                  this.props.pressed && styles.pressedMarkerStyle,
-                  this.props.pressed && this.props.pressedMarkerStyle,
-                ]
-              : [styles.markerStyle, styles.disabled]
-          }
-        />
+          style={this.props.enabled ? [
+            styles.markerStyle,
+            this.props.markerStyle,
+            this.props.pressed && styles.pressedMarkerStyle,
+            this.props.pressed && this.props.pressedMarkerStyle,
+          ] : [styles.markerStyle, styles.disabled]}
+        >
+
+          <Image style={{ width: 29, height: 26, marginTop: 5 }} source={require("./ruby.png")}></Image>
+        </View>
       </TouchableHighlight>
     );
   }
@@ -25,42 +38,43 @@ class DefaultMarker extends React.Component {
 const styles = StyleSheet.create({
   markerStyle: {
     ...Platform.select({
+      // 圆圈样式
       ios: {
-        height: 30,
-        width: 30,
-        borderRadius: 30,
-        borderWidth: 1,
-        borderColor: '#DDDDDD',
-        backgroundColor: '#FFFFFF',
-        shadowColor: '#000000',
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowRadius: 1,
-        shadowOpacity: 0.2,
+        // height: 30,
+        // width: 30,
+        // borderRadius: 30,
+        // borderColor: '#FF689A',
+        // borderWidth: 1,
+        // backgroundColor: '#FFFFFF',
+        // shadowColor: '#000000',
+        // shadowOffset: {
+        //   width: 0,
+        //   height: 3,
+        // },
+        // shadowRadius: 1,
+        // shadowOpacity: 0.2,
       },
       android: {
-        height: 12,
-        width: 12,
-        borderRadius: 12,
-        backgroundColor: '#0D8675',
+        // height: 30,
+        // width: 30,
+        // borderRadius: 30,
+        // backgroundColor: '#fff',
+        // borderColor: '#FF689A',
+        // borderWidth: 1,
       },
     }),
   },
   pressedMarkerStyle: {
-    ...Platform.select({
-      ios: {},
-      android: {
-        height: 20,
-        width: 20,
-        borderRadius: 20,
-      },
-    }),
+    // ...Platform.select({
+    //   ios: {},
+    //   android: {
+    //     height: 20,
+    //     width: 20,
+    //     borderRadius: 20,
+    //   },
+    // }),
   },
   disabled: {
     backgroundColor: '#d3d3d3',
   },
 });
-
-export default DefaultMarker;
